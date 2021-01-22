721731ceb2b92d1f7122ae60feda22e125c7bc02
Emily Qiu <eqiu@college.harvard.edu>
2021-01-22 12:49:43 -0500

    impt message

--- src/containers/Home/components/SearchResults/index.js
+++ src/containers/Home/components/SearchResults/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import { ListContainer, List, ListItem } from './styles'
 
-const API_KEY = '5dc246545b634cc48af9fb7a2a5e609a'
+const API_KEY = '5dc246545b634cc48af9fb7a2a5e609a' // ONLY CHANGE THIS ONE, OTHER TWO ARE FROM THE OTHER API
 const APP_ID = 'f1500858'
 const APP_KEY = '090812a8cbcf00e2831e04f48c0fa243'
 
